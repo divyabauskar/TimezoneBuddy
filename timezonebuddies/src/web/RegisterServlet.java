94fac8c106084ed372be9387df36e4551e852594
divyabauskar <divyabauskar@gmail.com>
2025-11-02 11:37:14 +0530

    Add files via upload

--- timezonebuddies/src/web/RegisterServlet.java
+++ timezonebuddies/src/web/RegisterServlet.java
@@ -1,5 +1,32 @@
+package com.project.web;
+
+
+
+
+
+
+import java.io.IOException;
+
+
+import jakarta.servlet.*;
+
+
+import jakarta.servlet.annotation.WebServlet;
+
+
+import jakarta.servlet.http.*;
+
+
+import com.project.dao.UserDao;
+
+
+import com.project.model.User;
+
+
+
 @WebServlet("/register")
 public class RegisterServlet extends HttpServlet {
+     private static final long serialVersionUID = 1L;
     private UserDao userDao;
 
     public void init() {
@@ -12,8 +39,28 @@ public class RegisterServlet extends HttpServlet {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
         String country = request.getParameter("country");
+        String timezone = request.getParameter("timezone");
+         User user = new User(name, email, username, password);
+
+        user.setTimezone(timezone);
+           
+         try {
+
+            userDao.registerUser(user);
+
+
+            response.sendRedirect("login.html");
+
+
+        } catch (Exception e) {
+
+
+            e.printStackTrace();
+
+
+        }
 
         userDao.saveUser(fullname, email, password, country);
-        response.sendRedirect("login.jsp");
+        response.sendRedirect("login.html");
     }
 }
